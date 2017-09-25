1dd418090ec18ee07efddb503691058860473434
aershov <codumentary.com@gmail.com>
2017-09-25 09:03:34 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='pymash',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'aiohttp==2.2.5',
+        'aiodns==1.1.1',
+        'cchardet==2.1.1',
+    ]
+)
