2ce09fc27dbca91bc97aafcf2cefae6091b3291c
aershov <codumentary.com@gmail.com>
2017-10-06 19:00:38 +0300

    add failing parser text

---
+++ src/pymash/parser.py
@@ -0,0 +1,8 @@
+class Function:
+    def __init__(self, name, text):
+        self.name = name
+        self.text = text
+
+
+def get_functions(source_code):
+    return []
---
+++ tests/test_parser.py
@@ -0,0 +1,29 @@
+import textwrap
+
+import pytest
+
+from pymash import parser
+
+
+@pytest.mark.parametrize('source_code, expected_functions', [
+    (
+            '''\
+            def add(x, y):
+                return x + y
+            ''',
+            [
+                parser.Function(
+                    name='add',
+                    text=textwrap.dedent(
+                        '''\
+                        def add(x, y):
+                            return x + y
+                        '''
+                    )
+                )
+            ]
+    )
+])
+def test_get_functions(source_code, expected_functions):
+    actual_functions = parser.get_functions(textwrap.dedent(source_code))
+    assert actual_functions == expected_functions
