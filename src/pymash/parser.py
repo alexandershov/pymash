bd8775eaa47fdfe06f1bad0488cd9cfbb054b08a
aershov <codumentary.com@gmail.com>
2018-01-01 11:35:56 +0300

    fix test

--- src/pymash/parser.py
+++ src/pymash/parser.py
@@ -4,7 +4,7 @@ import re
 # TODO(aershov182): better logging a whole project
 
 
-_MULTILINE_DOCSTRING_RE = re.compile('""".*?"""\n', re.DOTALL)
+_MULTILINE_DOCSTRING_RE = re.compile(r'[ \t]*""".*?"""\n', re.DOTALL)
 
 
 class _End:
