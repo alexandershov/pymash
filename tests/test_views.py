b62d144fb9350570f15a8bde3b0194ce5a66bf08
aershov <codumentary.com@gmail.com>
2018-01-03 08:32:03 +0300

    check db_name for sql injection. Just in case.

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,3 +1,4 @@
+import string
 import urllib.parse as urlparse
 
 import pytest
@@ -21,7 +22,7 @@ def _pymash_engine(request):
 def _get_engine(request, database=None):
     config = cfg.get_config()
     if database is not None:
-        dsn = _replace_dsn_database(config.dsn, database)
+        dsn = _replace_database_in_dsn(config.dsn, database)
     else:
         dsn = config.dsn
     engine = sa.create_engine(dsn)
@@ -29,7 +30,7 @@ def _get_engine(request, database=None):
     return engine
 
 
-def _replace_dsn_database(dsn, new_database):
+def _replace_database_in_dsn(dsn, new_database):
     parsed = urlparse.urlparse(dsn)
     replaced = parsed._replace(path=new_database)
     return replaced.geturl()
@@ -38,11 +39,9 @@ def _replace_dsn_database(dsn, new_database):
 @pytest.fixture(scope='session', autouse=True)
 def _create_database(request, _system_engine):
     test_db_name = _get_test_db_name()
-    # TODO(aershov182): use sqlalchemy for query generation
-    with _system_engine.connect().execution_options(
-            isolation_level="AUTOCOMMIT") as conn:
-        conn.execute(f'DROP DATABASE IF EXISTS {test_db_name}')
-        conn.execute(f'CREATE DATABASE {test_db_name}')
+    _run_system_commands(
+        _system_engine,
+        _drop_db_stmt(test_db_name), _create_db_stmt(test_db_name))
     create_tables()
     request.addfinalizer(lambda: _drop_database(_system_engine))
 
@@ -54,6 +53,28 @@ def _drop_database(system_engine):
     with system_engine.connect().execution_options(
             isolation_level="AUTOCOMMIT") as conn:
         conn.execute(f'DROP DATABASE {test_db_name}')
+
+
+def _run_system_commands(system_engine, *commands):
+    # we need isolation_level="AUTOCOMMIT", because CREATE/DROP DATABASE can't run in a transaction
+    with system_engine.connect().execution_options(isolation_level="AUTOCOMMIT") as conn:
+        for a_command in commands:
+            conn.execute(a_command)
+
+
+def _drop_db_stmt(db_name):
+    assert _is_valid_db_name(db_name)
+    return f'DROP DATABASE IF EXISTS {db_name}'
+
+
+def _create_db_stmt(db_name):
+    assert _is_valid_db_name(db_name)
+    return f'CREATE DATABASE {db_name}'
+
+
+def _is_valid_db_name(db_name):
+    allowed_characters = string.ascii_letters + '_'
+    return all(c in allowed_characters for c in db_name)
 
 
 def _get_test_db_name():
