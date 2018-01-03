f22e2af074ffa1011d75f08940b1a8e3b9235cdd
aershov <codumentary.com@gmail.com>
2018-01-03 12:06:06 +0300

    validate scores

--- src/pymash/events.py
+++ src/pymash/events.py
@@ -2,6 +2,6 @@ import decimal
 
 
 async def post_game_finished_event(game_id: int, white_id: int, black_id: int,
-                                   white_score: decimal.Decimal,
-                                   black_score: decimal.Decimal) -> None:
+                                   white_score: int,
+                                   black_score: int) -> None:
     raise NotImplementedError
--- src/pymash/views.py
+++ src/pymash/views.py
@@ -18,12 +18,19 @@ async def show_leaders(request: web.Request) -> dict:
 async def post_game(request: web.Request) -> web.Response:
     data = await request.post()
     game_id = request.match_info['game_id']
+    try:
+        white_score = int(data['white_score'])
+        black_score = int(data['black_score'])
+    except ValueError:
+        return web.HTTPBadRequest()
+    if white_score not in [0, 1] or black_score not in [0, 1] or white_score + black_score != 1:
+        return web.HTTPBadRequest()
     await events.post_game_finished_event(
         game_id=game_id,
         white_id=int(data['white_id']),
         black_id=int(data['black_id']),
-        white_score=decimal.Decimal(data['white_score']),
-        black_score=decimal.Decimal(data['black_score']))
+        white_score=white_score,
+        black_score=black_score)
     return web.Response(text='{}')
 
 
