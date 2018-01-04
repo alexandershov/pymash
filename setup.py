9d83029ab074e374d11cb88eb38a73753877d172
aershov <codumentary.com@gmail.com>
2018-01-04 10:54:12 +0300

    remove unused dependencies

--- setup.py
+++ setup.py
@@ -10,9 +10,6 @@ setuptools.setup(
         'aiohttp-jinja2==0.14.0',
         'aiodns==1.1.1',
         'aiopg==0.13.1',
-        # TODO(aershov182): do we need astroid & cchardet?
-        'astroid==1.5.3',
-        'cchardet==2.1.1',
         'voluptuous==0.10.5',
         'psycopg2==2.7.3.1',
         'SQLAlchemy==1.1.14',
