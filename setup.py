1ed5aa040cf5a3175708cc02e07bcc0e9011f48a
aershov <codumentary.com@gmail.com>
2018-01-04 21:25:16 +0300

    add cchardet to dependencies - it's an optional dependency for aiohttp

--- setup.py
+++ setup.py
@@ -10,9 +10,10 @@ setuptools.setup(
         'aiohttp-jinja2==0.14.0',
         'aiodns==1.1.1',
         'aiopg==0.13.1',
-        'voluptuous==0.10.5',
+        'cchardet==2.1.1',  # faster replacement for chardet, used by aiohttp
         'psycopg2==2.7.3.1',
         'SQLAlchemy==1.1.14',
+        'voluptuous==0.10.5',
     ],
     tests_require=[
         'pytest==3.2.2',
