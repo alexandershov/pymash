5f2a4c9785c2b3503e8907b0ec52b115ff89251b
aershov <codumentary.com@gmail.com>
2018-01-06 08:22:39 +0300

    stub to fix tests

--- src/pymash/events.py
+++ src/pymash/events.py
@@ -4,3 +4,7 @@ from pymash import models
 async def post_game_finished_event(game: models.Game) -> None:
     # TODO: implement it
     raise NotImplementedError
+
+
+def process_game_finished_event(game: models.Game) -> None:
+    pass
