96d6a36aaf387c1c99c42812de19cb506fb62999
aershov <codumentary.com@gmail.com>
2018-01-06 10:23:54 +0300

    rename white_function -> white_fn

--- src/pymash/events.py
+++ src/pymash/events.py
@@ -20,8 +20,8 @@ async def post_game_finished_event(game: models.Game) -> None:
 
 def process_game_finished_event(engine, game: models.Game) -> None:
     try:
-        white_function, black_function = db.find_many_functions_by_ids(engine, [game.white_id, game.black_id])
-        white_repo, black_repo = db.find_many_repos_by_ids(engine, [white_function.repo_id, black_function.repo_id])
+        white_fn, black_fn = db.find_many_functions_by_ids(engine, [game.white_id, game.black_id])
+        white_repo, black_repo = db.find_many_repos_by_ids(engine, [white_fn.repo_id, black_fn.repo_id])
     except db.NotFound:
         raise NotFound
     match = models.Match(white_repo, black_repo, game.result)
