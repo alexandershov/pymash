8cb4288925898374c3cc46beb8e20c5742e2f74c
aershov <codumentary.com@gmail.com>
2018-01-06 09:15:08 +0300

    handle not found

--- src/pymash/events.py
+++ src/pymash/events.py
@@ -2,14 +2,25 @@ from pymash import db
 from pymash import models
 
 
+class BaseError(Exception):
+    pass
+
+
+class NotFound(BaseError):
+    pass
+
+
 async def post_game_finished_event(game: models.Game) -> None:
     # TODO: implement it
     raise NotImplementedError
 
 
 def process_game_finished_event(engine, game: models.Game) -> None:
-    white_function, black_function = db.find_many_functions_by_ids(engine, [game.white_id, game.black_id])
-    white_repo, black_repo = db.find_many_repos_by_ids(engine, [white_function.repo_id, black_function.repo_id])
+    try:
+        white_function, black_function = db.find_many_functions_by_ids(engine, [game.white_id, game.black_id])
+        white_repo, black_repo = db.find_many_repos_by_ids(engine, [white_function.repo_id, black_function.repo_id])
+    except db.NotFound:
+        raise NotFound
     match = models.Match(white_repo, black_repo, game.result)
     match.change_ratings()
     db.insert_game(engine, game)
--- tests/test_events.py
+++ tests/test_events.py
@@ -14,6 +14,16 @@ def test_process_game_finished_event(pymash_engine):
     _assert_repo_has_rating(pymash_engine, repo_id=2, expected_rating=1908.63)
 
 
+def test_process_game_finished_event_unknown_white_id(pymash_engine):
+    _add_data(pymash_engine)
+    game = _get_game(white_id=1000000)
+    with pytest.raises(events.NotFound):
+        events.process_game_finished_event(pymash_engine, game)
+    _assert_game_not_saved(pymash_engine, game)
+    _assert_repo_has_rating(pymash_engine, repo_id=1, expected_rating=1800)
+    _assert_repo_has_rating(pymash_engine, repo_id=2, expected_rating=1900)
+
+
 # TODO: remove duplication with test_views.py
 def _add_data(pymash_engine):
     with pymash_engine.connect() as conn:
@@ -71,3 +81,9 @@ def _assert_game_saved(pymash_engine, game):
     assert row[Games.c.black_id] == game.black_id
     assert row[Games.c.white_score] == game.result.white_score
     assert row[Games.c.black_score] == game.result.black_score
+
+
+def _assert_game_not_saved(pymash_engine, game):
+    with pymash_engine.connect() as conn:
+        rows = list(conn.execute(Games.select().where(Games.c.game_id == game.game_id)))
+    assert len(rows) == 0
