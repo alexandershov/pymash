2451b2ed051a207ba97b09a7b59250c4f88e08b5
aershov <codumentary.com@gmail.com>
2018-01-07 08:26:15 +0300

    parse aws-related environment variables

--- src/pymash/cfg.py
+++ src/pymash/cfg.py
@@ -3,8 +3,10 @@ import os
 import voluptuous as vol
 
 _PYMASH_GAME_HASH_SALT_ENV_KEY = 'PYMASH_GAME_HASH_SALT'
-
 _PYMASH_DSN_ENV_KEY = 'PYMASH_DSN'
+_PYMASH_AWS_REGION = 'PYMASH_AWS_REGION'
+_PYMASH_AWS_ACCESS_KEY_ID = 'PYMASH_AWS_ACCESS_KEY_ID'
+_PYMASH_AWS_SECRET_ACCESS_KEY = 'PYMASH_AWS_SECRET_ACCESS_KEY'
 
 
 class BaseError(Exception):
@@ -18,7 +20,10 @@ class ConfigError(BaseError):
 _ENV_CONFIG_SCHEMA = vol.Schema(
     {
         _PYMASH_DSN_ENV_KEY: str,
-        _PYMASH_GAME_HASH_SALT_ENV_KEY: str
+        _PYMASH_GAME_HASH_SALT_ENV_KEY: str,
+        _PYMASH_AWS_REGION: str,
+        _PYMASH_AWS_ACCESS_KEY_ID: str,
+        _PYMASH_AWS_SECRET_ACCESS_KEY: str,
     },
     required=True, extra=vol.ALLOW_EXTRA)
 
@@ -30,13 +35,20 @@ def get_config():
         raise ConfigError from exc
     return Config(
         dsn=parsed_config[_PYMASH_DSN_ENV_KEY],
-        game_hash_salt=parsed_config[_PYMASH_GAME_HASH_SALT_ENV_KEY])
+        game_hash_salt=parsed_config[_PYMASH_GAME_HASH_SALT_ENV_KEY],
+        aws_region=parsed_config[_PYMASH_AWS_REGION],
+        aws_access_key_id=parsed_config[_PYMASH_AWS_ACCESS_KEY_ID],
+        aws_secret_access_key=parsed_config[_PYMASH_AWS_SECRET_ACCESS_KEY])
 
 
 class Config:
-    def __init__(self, dsn: str, game_hash_salt: str):
+    def __init__(self, dsn: str, game_hash_salt: str, aws_region: str, aws_access_key_id: str,
+                 aws_secret_access_key: str):
         self.dsn = dsn
         self.game_hash_salt = game_hash_salt
+        self.aws_region = aws_region
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
 
 
 def _get_env(name, parser):
