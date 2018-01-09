d805f14ed10139d29cfad0ce1b78bba6f150c2c2
aershov <codumentary.com@gmail.com>
2018-01-09 09:06:11 +0300

    add boto3 dependency

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setuptools.setup(
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
+        'aioboto3==2.0.1',
+        'aiodns==1.1.1',
         'aiohttp==2.2.5',
         'aiohttp-jinja2==0.14.0',
-        'aiodns==1.1.1',
         'aiopg==0.13.1',
         'cchardet==2.1.1',  # faster replacement for chardet, used by aiohttp
         'psycopg2==2.7.3.1',
