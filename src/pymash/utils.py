f75abdfd569373f38d0a51b1083c2bbc50b734b9
aershov <codumentary.com@gmail.com>
2018-01-10 09:04:33 +0300

    fix IntelliJ warnings

--- src/pymash/utils.py
+++ src/pymash/utils.py
@@ -4,25 +4,23 @@ import time
 import asyncio
 
 
-class log_time:
-    def __init__(self, logger):
-        self._logger = logger
-
-    # TODO: DRY it up
-    def __call__(self, fn):
+def log_time(logger):
+    def decorator(fn):
         if asyncio.iscoroutinefunction(fn):
             async def wrapper(*args, **kwargs):
                 started_at = time.time()
                 result = await fn(*args, **kwargs)
                 finished_at = time.time()
-                self._logger.info('%s.%s took %.3fs', fn.__module__, fn.__qualname__, finished_at - started_at)
+                logger.info('%s.%s took %.3fs', fn.__module__, fn.__qualname__, finished_at - started_at)
                 return result
         else:
             def wrapper(*args, **kwargs):
                 started_at = time.time()
                 result = fn(*args, **kwargs)
                 finished_at = time.time()
-                self._logger.info('%s.%s took %.3fs', fn.__module__, fn.__qualname__, finished_at - started_at)
+                logger.info('%s.%s took %.3fs', fn.__module__, fn.__qualname__, finished_at - started_at)
                 return result
 
         return functools.update_wrapper(wrapper, fn)
+
+    return decorator
