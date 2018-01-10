2cd28786dc6f8dc55b105a2ffe9584dba0ea7abd
aershov <codumentary.com@gmail.com>
2018-01-10 09:07:23 +0300

    use `url` context function to generate urls

--- src/pymash/routes.py
+++ src/pymash/routes.py
@@ -5,4 +5,4 @@ from pymash import views
 def setup_routes(app: web.Application):
     app.router.add_get('/game', views.show_game, name='new_game')
     app.router.add_post('/game/{game_id}', views.post_game)
-    app.router.add_get('/leaders', views.show_leaders)
+    app.router.add_get('/leaders', views.show_leaders, name='show_leaders')
