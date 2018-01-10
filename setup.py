83fbf9040fd9dc7e70c0cc52614820173b62e608
aershov <codumentary.com@gmail.com>
2018-01-10 08:33:14 +0300

    take load_data_from_github parameters from cmd args

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pymash',
-    version='0.1.4',
+    version='0.1.5',
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
--- src/pymash/scripts/load_data_from_github.py
+++ src/pymash/scripts/load_data_from_github.py
@@ -1,16 +1,29 @@
+import argparse
+
 import sqlalchemy as sa
+
 from pymash import cfg
 from pymash import loader
 
 
 def main():
     # TODO: dry it up with create_db.py
+    args = _parse_args()
     config = cfg.get_config()
     engine = sa.create_engine(config.dsn)
     loader.load_most_popular(
-        engine, 'python', 10,
+        engine,
+        language=args.language,
+        limit=args.limit,
         extra_repos_full_names=['alexandershov/pymash'])
     engine.dispose()
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('language')
+    parser.add_argument('limit', type=int)
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
