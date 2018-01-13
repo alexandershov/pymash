5a793c30d5c4e31a81dd1d398881059aae4bc22a
aershov <codumentary.com@gmail.com>
2018-01-13 15:51:10 +0300

    serve static via aiohttp

--- src/pymash/routes.py
+++ src/pymash/routes.py
@@ -1,3 +1,5 @@
+import os
+
 from aiohttp import web
 from pymash import views
 
@@ -6,3 +8,4 @@ def setup_routes(app: web.Application):
     app.router.add_get('/game', views.show_game, name='new_game')
     app.router.add_post('/game/{game_id}', views.post_game)
     app.router.add_get('/leaders', views.show_leaders, name='show_leaders')
+    app.router.add_static('/static', os.path.join(os.path.dirname(__file__), 'static'))
