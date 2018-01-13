14cc4c8f77d9fe69f56c4203b0b887681e144b3d
aershov <codumentary.com@gmail.com>
2018-01-13 15:53:42 +0300

    move static folder

--- src/pymash/routes.py
+++ src/pymash/routes.py
@@ -8,4 +8,4 @@ def setup_routes(app: web.Application):
     app.router.add_get('/game', views.show_game, name='new_game')
     app.router.add_post('/game/{game_id}', views.post_game)
     app.router.add_get('/leaders', views.show_leaders, name='show_leaders')
-    app.router.add_static('/static', os.path.join(os.path.dirname(__file__), 'static'))
+    app.router.add_static('/static', os.path.join(os.path.dirname(__file__), 'templates', 'static'))
