4d203a412af68ddea373aecb6fa38adaf45ffead
aershov <codumentary.com@gmail.com>
2018-01-14 12:17:42 +0300

    break line for better readability

--- src/pymash/cfg.py
+++ src/pymash/cfg.py
@@ -35,8 +35,9 @@ _ENV_CONFIG_SCHEMA = vol.Schema(
 
 
 class Config:
-    def __init__(self, dsn: str, game_hash_salt: str, aws_region_name: str, aws_access_key_id: str,
-                 aws_secret_access_key: str, sqs_games_queue_name: str, github_token: str) -> None:
+    def __init__(
+            self, dsn: str, game_hash_salt: str, aws_region_name: str, aws_access_key_id: str,
+            aws_secret_access_key: str, sqs_games_queue_name: str, github_token: str) -> None:
         self.dsn = dsn
         self.game_hash_salt = game_hash_salt
         self.aws_region_name = aws_region_name
