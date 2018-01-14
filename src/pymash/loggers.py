12a720f77641ab7bc6dcf9794fcee8601d6cfe9c
aershov <codumentary.com@gmail.com>
2018-01-14 19:44:37 +0300

    add time to logging

--- src/pymash/loggers.py
+++ src/pymash/loggers.py
@@ -6,4 +6,6 @@ loader = logging.getLogger('pymash.loader')
 
 
 def setup_logging():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s %(message)s')
