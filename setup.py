21e6183d5ab415fab2a039092af91e52c75cc18d
aershov <codumentary.com@gmail.com>
2018-01-17 23:06:56 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pymash',
-    version='0.1.7',
+    version='0.1.8',
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
