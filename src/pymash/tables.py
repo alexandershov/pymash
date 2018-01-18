17a5af2f4053f6ebeea3eafb8d0fd9adfee87e65
Alexander Ershov <aershov182@yandex-team.ru>
2018-01-18 23:49:07 +0300

    clean up

--- src/pymash/tables.py
+++ src/pymash/tables.py
@@ -4,16 +4,6 @@ from sqlalchemy import event
 
 __all__ = ['Repos', 'Functions', 'Games']
 
-
-def get_index_by_name(table, name):
-    for index in table.indexes:
-        if index.name == name:
-            return index
-    raise ValueError(f'unknown index {name}')
-
-
-Base = declarative.declarative_base()
-
 _TRIGGER_TEMPLATE = (
     '''CREATE FUNCTION set_updated_{table_name}()                                                                           returns trigger as $$
        BEGIN
@@ -25,8 +15,32 @@ _TRIGGER_TEMPLATE = (
     'FOR EACH ROW EXECUTE PROCEDURE set_updated_{table_name}()')
 
 
-# TODO: add created/updated attributes to all tables
-class _RepoDbModel(Base):
+def get_index_by_name(table, name):
+    for index in table.indexes:
+        if index.name == name:
+            return index
+    raise ValueError(f'unknown index {name}')
+
+
+def _get_table_with_trigger(model):
+    table = model.__table__
+    trigger = sa.DDL(_TRIGGER_TEMPLATE.format(table_name=model.__tablename__))
+    event.listen(table, 'after_create', trigger)
+    return table
+
+
+Base = declarative.declarative_base()
+
+
+class _CreatedUpdatedMixin:
+    created = sa.Column(
+        sa.DateTime(timezone=True),
+        server_default=sa.func.current_timestamp(),
+        nullable=False)
+    updated = sa.Column(sa.DateTime(timezone=True), nullable=False)
+
+
+class _RepoDbModel(_CreatedUpdatedMixin, Base):
     __tablename__ = 'repos'
     repo_id = sa.Column(sa.BigInteger, primary_key=True, nullable=False)
     github_id = sa.Column(sa.BigInteger, unique=True, index=True, nullable=False)
@@ -34,12 +48,6 @@ class _RepoDbModel(Base):
     url = sa.Column(sa.Text, nullable=False)
     is_active = sa.Column(sa.Boolean, nullable=False)
     rating = sa.Column(sa.Float, nullable=False)
-    # TODO(aershov182): dry created/updated definition in all tables
-    created = sa.Column(
-        sa.DateTime(timezone=True),
-        server_default=sa.func.current_timestamp(),
-        nullable=False)
-    updated = sa.Column(sa.DateTime(timezone=True), nullable=False)
 
     __table_args__ = (
         sa.Index(
@@ -49,25 +57,16 @@ class _RepoDbModel(Base):
     )
 
 
-Repos = _RepoDbModel.__table__
-
-repos_trigger = sa.DDL(
-    _TRIGGER_TEMPLATE.format(table_name='repos'))
-event.listen(Repos, 'after_create', repos_trigger)
+Repos = _get_table_with_trigger(_RepoDbModel)
 
 
-class _FunctionDbModel(Base):
+class _FunctionDbModel(_CreatedUpdatedMixin, Base):
     __tablename__ = 'functions'
     function_id = sa.Column(sa.BigInteger, primary_key=True, nullable=False)
     repo_id = sa.Column(sa.ForeignKey(Repos.c.repo_id), nullable=False, index=True)
     text = sa.Column(sa.Text, nullable=False)
     is_active = sa.Column(sa.Boolean, nullable=False)
     random = sa.Column(sa.Float, server_default=sa.func.random(), nullable=False, index=True)
-    created = sa.Column(
-        sa.DateTime(timezone=True),
-        server_default=sa.func.current_timestamp(),
-        nullable=False)
-    updated = sa.Column(sa.DateTime(timezone=True), nullable=False)
 
     __table_args__ = (
         sa.Index(
@@ -81,27 +80,16 @@ class _FunctionDbModel(Base):
     )
 
 
-Functions = _FunctionDbModel.__table__
-functions_trigger = sa.DDL(
-    _TRIGGER_TEMPLATE.format(table_name='functions'))
-event.listen(Functions, 'after_create', functions_trigger)
+Functions = _get_table_with_trigger(_FunctionDbModel)
 
 
-class _GameDbModel(Base):
+class _GameDbModel(_CreatedUpdatedMixin, Base):
     __tablename__ = 'games'
     game_id = sa.Column(sa.Text, primary_key=True, nullable=False)
     white_id = sa.Column(sa.ForeignKey(Functions.c.function_id), nullable=False)
     black_id = sa.Column(sa.ForeignKey(Functions.c.function_id), nullable=False)
     white_score = sa.Column(sa.Integer, nullable=False)
     black_score = sa.Column(sa.Integer, nullable=False)
-    created = sa.Column(
-        sa.DateTime(timezone=True),
-        server_default=sa.func.current_timestamp(),
-        nullable=False)
-    updated = sa.Column(sa.DateTime(timezone=True), nullable=False)
 
 
-Games = _GameDbModel.__table__
-games_trigger = sa.DDL(
-    _TRIGGER_TEMPLATE.format(table_name='games'))
-event.listen(Games, 'after_create', games_trigger)
+Games = _get_table_with_trigger(_GameDbModel)
