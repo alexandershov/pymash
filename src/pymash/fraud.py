9b02bf03d66835a5dd2b0b0909e798e4b27b1902
aershov <codumentary.com@gmail.com>
2018-02-01 23:27:36 +0300

    going forward

--- src/pymash/fraud.py
+++ src/pymash/fraud.py
@@ -6,51 +6,64 @@ from pymash import loggers
 from pymash import models
 
 
-class BaseDetails:
-    @property
-    def is_fraud(self):
+class BanDetails:
+    def is_banned_at(self, datetime: dt.datetime) -> bool:
         raise NotImplementedError
 
 
-class GoodDetails(BaseDetails):
-    @property
-    def is_fraud(self):
+class NotBannedDetails(BanDetails):
+    def is_banned_at(self, datetime: dt.datetime) -> bool:
         return False
 
     def __repr__(self):
-        return f'GoodDetails()'
+        return f'NotBannedDetails()'
 
 
-class BadDetails(BaseDetails):
-    def __init__(self, reason):
+class BannedDetails(BanDetails):
+    def __init__(self, end, reason):
+        self._end = end
         self._reason = reason
 
-    @property
-    def is_fraud(self):
-        return True
+    def is_banned_at(self, datetime: dt.datetime) -> bool:
+        return self._end < datetime
 
     def __repr__(self):
-        return f'BadDetails(reason={self._reason!a})'
+        return f'BannedDetails(end={self._end!a}, reason={self._reason!a})'
 
 
 # TODO: expire old records
 class Watchman:
-    def __init__(self, rate_limit: float, window: dt.timedelta, ban_duration: dt.timedelta) -> None:
+    def __init__(self, rate_limit: float, window: dt.timedelta, ban_duration: dt.timedelta,
+                 num_attempts_before_gc: int = 1_000_000) -> None:
         assert window.total_seconds() >= 1
+        assert window.total_seconds().is_integer()
         self._rate_limit = rate_limit
         self._window = window
         self._ban_duration = ban_duration
-        self._attempts_by_ip: tp.Dict[str, tp.List[models.GameAttempt]] = collections.defaultdict(list)
+        self._attempts_by_ip: tp.Dict[str, _IpInfo] = collections.defaultdict(_IpInfo)
+        self._num_attempts_before_gc = num_attempts_before_gc
 
     def add(self, attempt: models.GameAttempt) -> None:
-        self._attempts_by_ip[attempt.ip].append(attempt)
+        info = self._attempts_by_ip[attempt.ip]
+        info.add(attempt.at)
+        cur = attempt.at - self._window
+        now = dt.datetime.utcnow()
+        if info.is_banned_at(now):
+            return
+        while cur <= attempt.at + self._window:
+            count = info.get_count_in_interval(cur, cur + self._window)
+            cur_rate = count / self._window.total_seconds()
+            if cur_rate > self._rate_limit:
+                reason = f'cur_rate is {cur_rate}, rate_limit is {self._rate_limit}'
+                info.ban(dt.datetime.utcnow() + self._ban_duration, reason)
+            cur += dt.timedelta(seconds=1)
 
-    def get_fraud_details(self, ip: str) -> BaseDetails:
+    def get_ban_details(self, ip: str) -> BanDetails:
         rate = self._get_cur_rate(ip)
         loggers.games_queue.info('ip %s has rate %s, rate_limit is %s', ip, rate, self._rate_limit)
         if rate > self._rate_limit:
-            return BadDetails(f'{rate} > {self._rate_limit}')
-        return GoodDetails()
+            return BannedDetails(f'{rate} > {self._rate_limit}')
+        return NotBannedDetails()
 
     def _get_cur_rate(self, ip) -> float:
         min_at = dt.datetime.utcnow() - self._window
@@ -59,3 +72,31 @@ class Watchman:
             if attempt.at >= min_at:
                 num_attempts += 1
         return num_attempts / self._window.total_seconds()
+
+
+class _IpInfo:
+    def __init__(self):
+        self._count_by_second = collections.Counter()
+        self._ban_details = NotBannedDetails()
+
+    def add(self, datetime: dt.datetime) -> None:
+        self._count_by_second[_convert_to_unix_ts(datetime)] += 1
+
+    def ban(self, end, reason) -> None:
+        self._ban_details = BannedDetails(end, reason)
+
+    def is_banned_at(self, datetime):
+        return self._ban_details.is_banned_at(datetime)
+
+    def get_count_in_interval(self, start: dt.datetime, end: dt.datetime) -> int:
+        result = 0
+        start_ts = _convert_to_unix_ts(start)
+        end_ts = _convert_to_unix_ts(end)
+        for ts, count in self._count_by_second.items():
+            if start_ts <= ts < end_ts:
+                result += count
+        return result
+
+
+def _convert_to_unix_ts(datetime: dt.datetime) -> int:
+    return int(datetime.replace(tzinfo=dt.timezone.utc).timestamp())
--- tests/test_antifraud.py
+++ tests/test_antifraud.py
@@ -13,23 +13,34 @@ _NOW = dt.datetime(2018, 1, 31, 19, 30, 27)
 @freezegun.freeze_time(_NOW)
 def test_watchman():
     watchman = _get_watchman()
-    assert not watchman.get_fraud_details(_IP).is_fraud
+    assert not watchman.get_fraud_details(_IP).is_banned
     watchman.add(models.GameAttempt(_IP, dt.datetime(2018, 1, 31, 19, 30, 25)))
 
-    assert not watchman.get_fraud_details(_IP).is_fraud
+    assert not watchman.get_fraud_details(_IP).is_banned
     watchman.add(models.GameAttempt(_IP, dt.datetime(2018, 1, 31, 19, 30, 26)))
 
-    assert not watchman.get_fraud_details(_IP).is_fraud
+    assert not watchman.get_fraud_details(_IP).is_banned
     watchman.add(models.GameAttempt(_IP, dt.datetime(2018, 1, 31, 19, 30, 27)))
 
-    assert not watchman.get_fraud_details(_IP).is_fraud
+    assert not watchman.get_fraud_details(_IP).is_banned
     watchman.add(models.GameAttempt(_IP, dt.datetime(2018, 1, 31, 19, 30, 27)))
 
-    assert watchman.get_fraud_details(_IP).is_fraud
+    assert watchman.get_fraud_details(_IP).is_banned
+
+
+# @freezegun.freeze_time(_NOW)
+# def test_watchman_gc():
+#     watchman = _get_watchman()
+#     watchman.add(models.GameAttempt(_IP, dt.datetime(2018, 1, 31, 19, 30, 28)))
+#     watchman.add(models.GameAttempt(_IP, dt.datetime(2018, 1, 31, 19, 30, 29)))
+#     watchman.add(models.GameAttempt(_IP, dt.datetime(2018, 1, 31, 19, 30, 30)))
+#     freezegun.freeze_time(dt.datetime(2018, 1, 31, 19, 30, 35))
+#     watchman.add(models.GameAttempt(_IP, dt.datetime(2018, 1, 31, 19, 30, 31)))
 
 
 def _get_watchman():
     return fraud.Watchman(
         rate_limit=1,
         window=dt.timedelta(seconds=3),
-        ban_duration=dt.timedelta(minutes=30))
+        ban_duration=dt.timedelta(minutes=30),
+        num_attempts_before_gc=3)
