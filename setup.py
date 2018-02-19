03212d380d903311c14405dd13f99d9573b3e2ab
aershov <codumentary.com@gmail.com>
2018-02-19 09:01:03 +0300

    add systemd for monitoring

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pymash',
-    version='0.2.1',
+    version='0.2.2',
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
