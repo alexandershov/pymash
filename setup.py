d09a081f34ae71c65f0923a348c59d9703453615
aershov <codumentary.com@gmail.com>
2018-02-20 08:22:11 +0300

    move to us-west-2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pymash',
-    version='0.2.4',
+    version='0.2.5',
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
