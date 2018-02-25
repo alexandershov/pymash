1b9ecd82f103128164fdbc90a64c9a1b5946abd1
aershov <codumentary.com@gmail.com>
2018-02-25 08:21:12 +0300

    info -> user

--- src/pymash/fraud.py
+++ src/pymash/fraud.py
@@ -31,12 +31,12 @@ class _BannedDetails(_BanDetails):
         return f'_BannedDetails(end={self._end!a}, reason={self._reason!a})'
 
 
-class _IpInfo:
+class _User:
     def __init__(self):
         self._num_attempts_by_unix_ts = collections.Counter()
         self._ban_details = _NotBannedDetails()
 
-    def add(self, datetime: dt.datetime) -> None:
+    def on_attempt_at(self, datetime: dt.datetime) -> None:
         self._num_attempts_by_unix_ts[_convert_to_unix_ts(datetime)] += 1
 
     def ban(self, end, reason) -> None:
@@ -79,42 +79,42 @@ class Watchman(BaseWatchman):
         self._rate_limit = rate_limit
         self._window = window
         self._ban_duration = ban_duration
-        self._info_by_ip: tp.Dict[str, _IpInfo] = collections.defaultdict(_IpInfo)
+        self._user_by_ip: tp.Dict[str, _User] = collections.defaultdict(_User)
         self._num_attempts_without_gc = 0
         self._max_num_attempts_without_gc = max_num_attempts_without_gc
 
     def add(self, now: dt.datetime, attempt: models.GameAttempt) -> None:
         self._num_attempts_without_gc += 1
-        info = self._info_by_ip[attempt.ip]
-        info.add(attempt.at)
+        user = self._user_by_ip[attempt.ip]
+        user.on_attempt_at(attempt.at)
         for datetime in self._rate_affecting_datetimes(attempt):
-            rate = self._get_rate(info, start=datetime)
+            rate = self._get_rate(user, start=datetime)
             if rate > self._rate_limit:
-                self._ban(attempt=attempt, info=info, rate=rate, now=now)
+                self._ban(attempt=attempt, user=user, rate=rate, now=now)
 
         self._gc(now)
 
     def is_banned_at(self, ip: str, datetime: dt.datetime) -> bool:
-        info = self._info_by_ip[ip]
-        return info.is_banned_at(datetime)
+        user = self._user_by_ip[ip]
+        return user.is_banned_at(datetime)
 
     @property
     def num_ips(self) -> int:
-        return len(self._info_by_ip)
+        return len(self._user_by_ip)
 
     def _rate_affecting_datetimes(self, attempt: models.GameAttempt) -> tp.Iterable[dt.datetime]:
         at_exact_sec = attempt.at.replace(microsecond=0)
         start = at_exact_sec - self._window + dt.timedelta(seconds=1)
         yield from _datetimes_between(start, at_exact_sec, step=dt.timedelta(seconds=1))
 
-    def _get_rate(self, info: _IpInfo, start: dt.datetime) -> float:
-        count = info.get_num_attempts_in_interval(start, start + self._window)
+    def _get_rate(self, user: _User, start: dt.datetime) -> float:
+        count = user.get_num_attempts_in_interval(start, start + self._window)
         return count / self._window.total_seconds()
 
-    def _ban(self, attempt: models.GameAttempt, info: _IpInfo, rate: float, now: dt.datetime) -> None:
+    def _ban(self, attempt: models.GameAttempt, user: _User, rate: float, now: dt.datetime) -> None:
         reason = f'cur_rate is {rate}, rate_limit is {self._rate_limit}'
         end = now + self._ban_duration
-        info.ban(end, reason)
+        user.ban(end, reason)
         loggers.games_queue.info('pymash_event:banned_ip %s till %s because %s', attempt.ip, end, reason)
 
     def _needs_gc(self) -> bool:
@@ -123,9 +123,9 @@ class Watchman(BaseWatchman):
     def _gc(self, now: dt.datetime) -> None:
         if not self._needs_gc():
             return
-        for ip in list(self._info_by_ip):
+        for ip in list(self._user_by_ip):
             if not self.is_banned_at(ip, now):
-                del self._info_by_ip[ip]
+                del self._user_by_ip[ip]
         self._num_attempts_without_gc = 0
 
 
